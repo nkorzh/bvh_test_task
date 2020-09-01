9afb1d615c23b7b3f2b213f5f6695909fffc41d2
Дима Елагин <dimonssturbo@mail.ru>
2020-09-02 04:29:32 +0500

    render fix

--- Source.cpp
+++ Source.cpp
@@ -15,15 +15,6 @@
 #include<GL/glut.h>
 
 
-GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
-  {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
-  {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
-GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
-  {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
-  {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
-GLfloat v[8][3];  /* Will be filled in with X,Y,Z vertexes. */
-
-
 //Просто парсер, который заполняет структуру, делал по документации вордовой
 
 Tree BuildTree(const char* file) {
@@ -97,17 +88,6 @@ void drawGeometry(Tree tree)
         glVertex3f(tree.leafArr[i]->aabb1_min_or_v2.vec[0], tree.leafArr[i]->aabb1_min_or_v2.vec[1], tree.leafArr[i]->aabb1_min_or_v2.vec[2]);
         glEnd();
     }
-
-   /* for (int i = 0; i < 6; i++) {
-        glBegin(GL_QUADS);
-        glEnable(GL_NORMALIZE);
-        glNormal3fv(&(n[i][0]));
-        glVertex3fv(&v[faces[i][0]][0]);
-        glVertex3fv(&v[faces[i][1]][0]);
-        glVertex3fv(&v[faces[i][2]][0]);
-        glVertex3fv(&v[faces[i][3]][0]);
-        glEnd();
-    }*/
 }
 
 void display(void)
@@ -119,13 +99,6 @@ void display(void)
 
 void init()
 {
-
-    v[0][0] = v[1][0] = v[2][0] = v[3][0] = -1;
-    v[4][0] = v[5][0] = v[6][0] = v[7][0] = 1;
-    v[0][1] = v[1][1] = v[4][1] = v[5][1] = -1;
-    v[2][1] = v[3][1] = v[6][1] = v[7][1] = 1;
-    v[0][2] = v[3][2] = v[4][2] = v[7][2] = 1;
-    v[1][2] = v[2][2] = v[5][2] = v[6][2] = -1;
 
     GLfloat light_diffuse[] = { 1.0, 0.0, 0.0, 1.0 };  /* Red diffuse light. */
     GLfloat light_position[] = { 1.0f, 1.0f, 1.0f, 0.0f };
@@ -145,7 +118,7 @@ void init()
         0.0, 0.0, 0.0,      /* center is at (0,0,0) */
         0.0, 1.0, 0.0);      /* up is in positive Y direction */
 
-      /* Adjust cube position to be asthetic angle. */
+      /* Adjust cube position to be angle. */
     glTranslatef(0.0, -1.0, -1.0);
     glRotatef(0, 1.0, 0.0, 0.0);
     glRotatef(20, 0.0, 1.0, 0.0);
